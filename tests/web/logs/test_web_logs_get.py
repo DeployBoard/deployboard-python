9543e8b9c52f3b56eaef564d469eb750b92a0f73
Matt Karmazyn <matt@itskarma.wtf>
2021-02-19 11:56:15 -0500

    refactor and tests

--- tests/web/logs/test_web_logs_get.py
+++ tests/web/logs/test_web_logs_get.py
@@ -1,12 +1,12 @@
 import pytest
 from unittest.mock import patch
-from src.web.webroutes.analytics import get_logs, get_services
+from src.web.webroutes.logs import get_logs, get_services
 
 
 # TODO: This test could be better.
-@patch('webroutes.analytics.get_services')
-@patch('webroutes.analytics.get_logs')
-def test_web_analytics_get_success(mock_get_logs, mock_get_services, client, admin_token):
+@patch('webroutes.logs.get_services')
+@patch('webroutes.logs.get_logs')
+def test_web_logs_get_success(mock_get_logs, mock_get_services, client, admin_token):
     mock_get_logs.return_value.json.return_value = [
         {
             "_id": "5ff89d1f0ab150bbf4cccbe4",
@@ -52,13 +52,13 @@ def test_web_analytics_get_success(mock_get_logs, mock_get_services, client, adm
         session['logged_in'] = True
         session['exp'] = 999999999999999
         session['token'] = admin_token
-    response = client.get('/analytics/?daysago=7')
+    response = client.get('/logs/')
     assert response.status_code == 200
-    assert b'Analytics' in response.data
+    assert b'Logs' in response.data
 
 
 @patch('requests.get')
-def test_web_analytics_get_logs_success(mock, admin_token):
+def test_web_logs_get_logs_success(mock, admin_token):
     mock.return_value.status_code = 200
     mock.return_value.json.return_value = []
     response = get_logs('', admin_token)
@@ -66,14 +66,14 @@ def test_web_analytics_get_logs_success(mock, admin_token):
 
 
 @patch('requests.get', side_effect=Exception('mocked error'))
-def test_web_analytics_get_logs_exception(mock, admin_token):
+def test_web_logs_get_logs_exception(mock, admin_token):
     with pytest.raises(Exception) as excinfo:
         get_logs('', admin_token)
     assert str(excinfo.value) == 'mocked error'
 
 
 @patch('requests.get')
-def test_web_analytics_get_services_success(mock, admin_token):
+def test_web_logs_get_services_success(mock, admin_token):
     mock.return_value.status_code = 200
     mock.return_value.json.return_value = []
     response = get_services(admin_token)
@@ -81,7 +81,7 @@ def test_web_analytics_get_services_success(mock, admin_token):
 
 
 @patch('requests.get', side_effect=Exception('mocked error'))
-def test_web_analytics_get_services_exception(mock, admin_token):
+def test_web_logs_get_services_exception(mock, admin_token):
     with pytest.raises(Exception) as excinfo:
         get_services(admin_token)
     assert str(excinfo.value) == 'mocked error'
