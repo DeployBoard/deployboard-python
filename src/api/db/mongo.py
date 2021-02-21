e7f21b151bd0681a7f24b9c1a7a2e8077d70a859
Matt Karmazyn <matt@itskarma.wtf>
2021-02-21 06:45:12 -0500

    adding support for pymongo_inmemory so our tests don't hit the live db

--- src/api/db/mongo.py
+++ src/api/db/mongo.py
@@ -1,6 +1,10 @@
 import os
-from pymongo import MongoClient
 
-client = MongoClient(os.environ['MONGO_URI'])
+if 'DPB_ENV' in os.environ and os.environ['DPB_ENV'] == 'pytest':
+    from pymongo_inmemory import MongoClient
+    client = MongoClient()
+else:
+    from pymongo import MongoClient
+    client = MongoClient(os.environ['MONGO_URI'])
 
-db = client.deployboardtest
+db = client.deployboard
