17e2506b779405aff95d0981a35ae4ca01d3f35d
Matt Karmazyn <matt@itskarma.wtf>
2021-02-26 19:28:27 -0500

    adding logout tests

---
+++ tests/web/logout/test_web_logout_get.py
@@ -0,0 +1,26 @@
+from flask import request, url_for
+from urllib.parse import urlparse
+
+
+def test_web_logout_get_logged_in(client):
+    with client.session_transaction() as session:
+        session['logged_in'] = True
+        session['exp'] = 999999999999999
+    response = client.get('/logout/', follow_redirects=False)
+    assert response.status_code == 302
+    assert urlparse(response.location).path == url_for('login_page.login')
+
+
+def test_web_logout_get_not_logged_in(client):
+    response = client.get('/logout/', follow_redirects=False)
+    assert response.status_code == 302
+    assert urlparse(response.location).path == url_for('login_page.login')
+
+
+def test_web_logout_get_no_trailing_slash(client):
+    with client.session_transaction() as session:
+        session['logged_in'] = True
+        session['exp'] = 999999999999999
+    response = client.get('/logout', follow_redirects=False)
+    assert response.status_code == 308
+    assert urlparse(response.location).path == url_for('logout_page.logout')
