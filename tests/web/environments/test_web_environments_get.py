e2b6ecf51ee8b5de4e9d30046d2e6ce891b1682d
Matt Karmazyn <matt@itskarma.wtf>
2021-02-26 20:10:02 -0500

    tests mocking get_api

--- tests/web/environments/test_web_environments_get.py
+++ tests/web/environments/test_web_environments_get.py
@@ -5,37 +5,15 @@ from webutil.webapi import get_api
 
 @patch('webutil.webapi.get_api')
 def test_web_dashboard_get_success(mock, client, admin_token):
-    mock.return_value.json.return_value = [
-        {
-            "schema_version": 1,
-            "service": "Api",
-            "application": "Admin",
-            "account": "Example",
-            "tags": [
-                "python"
-            ],
-            "versions": [
-                {
-                    "environment": "Prod",
-                    "status": "Deployed",
-                    "version": "1.2.0",
-                    "timestamp": 1608623640,
-                    "custom": {
-                        "module": "foo",
-                        "color": "green"
-                    }
-                }
-            ],
-            "_id": "602ec3a41d6ef526c9362d42"
-        }
-    ]
+    mock.return_value.json.return_value = ["Prod, Test, Dev"]
     with client.session_transaction() as session:
         session['logged_in'] = True
         session['exp'] = 999999999999999
         session['token'] = admin_token
     response = client.get('/dashboard/')
     assert response.status_code == 200
-    assert b'Dashboard' in response.data
+    assert b'Environments' in response.data
+    assert b'Prod' in response.data
 
 
 @patch('webutil.webapi.get_api', side_effect=Exception('mocked error'))
