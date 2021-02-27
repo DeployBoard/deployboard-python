82ea183384310ba5eb04d2a13e1454e9ac46b351
Matt Karmazyn <matt@itskarma.wtf>
2021-02-26 21:01:39 -0500

    correcting test names

--- tests/web/environments/test_web_environments_get.py
+++ tests/web/environments/test_web_environments_get.py
@@ -4,24 +4,24 @@ from webutil.webapi import get_api
 
 
 @patch('webutil.webapi.get_api')
-def test_web_dashboard_get_success(mock, client, admin_token):
+def test_web_environments_get_success(mock, client, admin_token):
     mock.return_value.json.return_value = ["Prod, Test, Dev"]
     with client.session_transaction() as session:
         session['logged_in'] = True
         session['exp'] = 999999999999999
         session['token'] = admin_token
-    response = client.get('/dashboard/')
+    response = client.get('/settings/environments/')
     assert response.status_code == 200
     assert b'Environments' in response.data
     assert b'Prod' in response.data
 
 
 @patch('webutil.webapi.get_api', side_effect=Exception('mocked error'))
-def test_dashboard_get_services_exception(mock, client, admin_token):
+def test_environments_get_services_exception(mock, client, admin_token):
     with client.session_transaction() as session:
         session['logged_in'] = True
         session['exp'] = 999999999999999
         session['token'] = admin_token
-    response = client.get('/dashboard/')
+    response = client.get('/settings/environments/')
     assert response.status_code == 500
     assert b'mocked error' in response.data
