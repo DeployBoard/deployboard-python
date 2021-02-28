0a2a41753b7149b3ee8f7143609460e238ac2bae
Matt Karmazyn <matt@itskarma.wtf>
2021-02-28 09:44:16 -0500

    adding post_api util

--- src/web/webutil/webapi.py
+++ src/web/webutil/webapi.py
@@ -14,11 +14,30 @@ def get_api(route, token):
             headers={'Authorization': f'Bearer {token}'}
         )
         # Log our response for debugging.
+        logger.debug(f"api response: {response.json()}")
+    except Exception as error:
+        # Log error for debugging.
+        logger.error(f"api error: {error}")
+        # Re-raise the same error.
+        raise
+    return response.json()
+
+
+def post_api(route, token, data):
+    """
+    Post to api endpoint and return response.
+    """
+    try:
+        response = requests.post(
+            f'http://api:8081/{route}',
+            headers={'Authorization': f'Bearer {token}'},
+            json=data
+        )
+        # Log our response for debugging.
         logger.debug(f"deployments response: {response.json()}")
     except Exception as error:
         # Log error for debugging.
         logger.error(f"deployments error: {error}")
         # Re-raise the same error.
         raise
-
     return response.json()
