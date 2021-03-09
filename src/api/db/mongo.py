b2055f2da4ead52dda19e250c230553e5e16dea4
Matt Karmazyn <matt@itskarma.wtf>
2021-03-09 10:19:15 -0500

    convert config to func

--- src/api/db/mongo.py
+++ src/api/db/mongo.py
@@ -1,12 +1,12 @@
 from util.config import config
 
 # If you pass mongo_uri we use that.
-if config["MONGO_URI"]:
+if config("MONGO_URI"):
     from pymongo import MongoClient
-    client = MongoClient(config["MONGO_URI"])
+    client = MongoClient(config("MONGO_URI"))
 # Else we use inmemory. This is really just used for testing.
 else:
     from pymongo_inmemory import MongoClient
     client = MongoClient()
 
-db = client[config["MONGO_DATABASE"]]
+db = client[config("MONGO_DATABASE")]
--- src/api/util/config.py
+++ src/api/util/config.py
@@ -1,14 +1,21 @@
 import os
 
-# Initialize our config.
-config = {
-    "MONGO_URI": None,
-    "MONGO_DATABASE": "deployboard"
-}
 
-# Loop through our defaults.
-for item in config:
-    # Check if the item is in the environment.
-    if item in os.environ:
-        # Override the default with what is in the environment.
-        config[item] = os.environ[item]
+def config(param):
+    """
+    Returns our config object after reading environment for overrides.
+    """
+    # Initialize our config.
+    config_dic = {
+        "MONGO_URI": None,
+        "MONGO_DATABASE": "deployboard"
+    }
+
+    # Loop through our defaults.
+    for item in config_dic:
+        # Check if the item is in the environment.
+        if item in os.environ:
+            # Override the default with what is in the environment.
+            config_dic[item] = os.environ[item]
+
+    return config_dic[param]
