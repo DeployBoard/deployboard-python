6e65400ca51e80cd647ec7e7d72a2c2d838f0cd0
Matt Karmazyn <matt@itskarma.wtf>
2021-03-12 19:51:13 -0500

    trailing slash like all other api routes

--- src/api/routes/token.py
+++ src/api/routes/token.py
@@ -2,18 +2,21 @@ from fastapi import APIRouter, HTTPException, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from util.auth import authenticate_user, create_access_token
 from models.auth import Token
+from datetime import timedelta
 
-from datetime import datetime, timedelta
+import logging
+logger = logging.getLogger(__name__)
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 480  # 8 hours
 
 router = APIRouter(
+    prefix="/token",
     tags=["Auth"],
     responses={404: {"description": "Not found"}}
 )
 
 
-@router.post("/token", response_model=Token)
+@router.post("/", response_model=Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
     user = authenticate_user(form_data.username, form_data.password)
     if not user:
